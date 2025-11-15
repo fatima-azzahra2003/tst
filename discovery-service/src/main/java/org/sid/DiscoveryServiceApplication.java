a0628a81d6bdcbee9e9d190bad7b0fe9d313c71a
fatima-azzahra2003 <fatimaazzahrahamdaoui3@gmail.com>
2025-11-15 21:54:40 +0100

    first commit

---
+++ discovery-service/src/main/java/org/sid/DiscoveryServiceApplication.java
@@ -0,0 +1,20 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Classe principale pour le service de découverte.
+ * L'annotation @EnableEurekaServer transforme cette application Spring Boot
+ * en un serveur Eureka.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+
+}
